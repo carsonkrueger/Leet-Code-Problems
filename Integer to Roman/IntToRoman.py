f83751112c0ba3a615ff27539568826fc40afcf5
carsonkrueger <crkruege@gmail.com>
2023-05-25 16:33:18 -0600

    int to roman complete

--- Integer to Roman/IntToRoman.py
+++ Integer to Roman/IntToRoman.py
@@ -1,13 +1,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = {"M":1000, "CM":900, "D":500, "CD":400, "6":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
+        symbols = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
         res: str= ""
-        for i in range(len(symbols)):
-            if (symbols[i] <= num):
-                res += symbols[i]
-                i -= 1
+        while (num != 0):
+            for ch in symbols:
+                val = symbols[ch]
+                while val <= num:
+                    num -= val
+                    res += ch
         return res
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.intToRoman(1994))
+    print(sol.intToRoman(20))
