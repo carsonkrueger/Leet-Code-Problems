e9e47a1ae70a347c257939b498385f2c7927365d
carsonkrueger <crkruege@gmail.com>
2023-06-11 21:42:39 -0600

    finished longest prefix problem

---
+++ longestPrefix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        i = 0
+        totalStr = ""
+        while True:
+            try:
+                curLetter = strs[0][i]
+                for s in strs:
+                    if curLetter != s[i]:
+                        return totalStr
+                i += 1
+                totalStr += curLetter
+
+            except IndexError:
+                return totalStr
+                    
+
+if __name__ == "__main__":
+    strs = ["f","flow","flight"]
+    sol = Solution
+    print(sol.longestCommonPrefix(sol, strs=strs))
